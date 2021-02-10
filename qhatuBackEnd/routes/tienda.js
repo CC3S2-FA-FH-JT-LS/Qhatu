b6548247d984600efe017337e020b21904051a2f
Yuma990 <fherviasa@uni.pe>
2021-02-10 16:49:37 -0500

    get product by id added

--- qhatuBackEnd/routes/tienda.js
+++ qhatuBackEnd/routes/tienda.js
@@ -6,5 +6,9 @@ api.post('/contador-contacto', tiendaControllers.contadorContacto);
 api.get('/mostrar-detalles-tienda', tiendaControllers.mostrarDetallesTienda);
 api.get('/obtener-productos', tiendaControllers.obtenerProductos);
 api.get('/obtener-comentarios', tiendaControllers.obtenerComentarios);
+api.get(
+  '/obtener-producto-por-id/:productoId',
+  tiendaControllers.getOneProductoById
+);
 
 module.exports = api;

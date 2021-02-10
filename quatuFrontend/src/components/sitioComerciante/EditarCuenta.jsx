1f82a9c00a6bcdf6e466a1ebd0e0e292ec62a6a9
FRD898 <freider-8@hotmail.com>
2021-02-10 16:39:28 -0500

    update account

--- quatuFrontend/src/components/sitioComerciante/EditarCuenta.jsx
+++ quatuFrontend/src/components/sitioComerciante/EditarCuenta.jsx
@@ -4,112 +4,23 @@ import ComercianteModels from "./modelData/Comerciante";
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 //import AppBarNav from "./AppBarNav";
-class Test extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      User: {
-        nombre: "",
-        nombreUsuario: "",
-        contacto: "",
-        numeroPuesto: "",
-        nombreTienda: "",
-        informacionPuesto: "",
-        categoria: "abarrotes",
-        imagen: "https://source.unsplash.com/random",
-      }
-    };
-    this.handleMount = this.handleMount.bind(this);
-  }
-<<<<<<< HEAD
-  handleMount(data){
-    console.log(data)
-=======
-  handleMount(event,data){
->>>>>>> adb3d0e2991c2e7598b355cc369fe56af5709eb6
-    this.setState({User:data});
-    console.log("-------");
-    console.log(this.state.User);
-  }
-  componentDidMount(){
-    var data={};
-    let tienda_id = localStorage.getItem('myStore');
-    let role = localStorage.getItem('myRole');
-    if(role === "comerciante"){
-      axios.get("/api/mostrar-detalles-tienda",
-      {params:{
-      tiendaId : "600046678f25c125841686ad",
-      }
-      }
-      ).then((res)=>{
-        console.log("obtenido: ");
-        console.log(res.data);
-
-        let datosTienda = res.data.response
-        let comerciante = datosTienda.comercianteId
-        //console.log( "La tieenda \n", laTienda);
-        //console.log( "El comercianteeee \n", miComerciante);
-        data = {
-          nombre: comerciante.nombre,
-          nombreUsuario: comerciante.nombreUsuario,
-          contacto: comerciante.contacto,
-          numeroPuesto: datosTienda.numeroPuesto,
-          nombreTienda: comerciante.nombreTienda,
-          informacionPuesto: datosTienda.informacionPuesto,
-          categoria: datosTienda.categoria,
-          imagen: datosTienda.imagen,
-        };
-        //console.log(data)
-        //console.log(usuarioComerciante)
-        
-        //usuarioComerciante = data;
-        //setComerciante(data);
-      })
-      .then(()=>{
-        console.log("data in setting state");
-        console.log(data);
-        /* this.setState({User:data}) */
-        this.handleMount(data);
-        //this.setState({User:data});
-        //this.render();
-      })
-      .catch(error => {
-        console.error('There was an error!', error);
-      });
-      //console.log("data after get");
-      //console.log(data);
-      //this.handleMount(data);
-    }    
-  }
-  
-  render() {
-    return (
-      <div>
-        <FormularioComerciante
-          componentTitle="EditarCuenta"
-          comerciante={this.state.User}
-          showDelBut="visible"
-        ></FormularioComerciante>
-      </div>
-    );
-  }
-}
 
 export default function EditarCuenta() {
-  //var [usuarioComerciante, setComerciante] = useState({});
-  //var usuarioComerciante;
-  //var data="";
-  //usuarioComerciante = useRef({});
-  /* const putData = function(data){
-    console.log("update user")
-    setComerciante(data)
-    console.log(usuarioComerciante.informacionPuesto)
-  } */
-  /* const setter = function(){
-    var data="";
+  const [usuarioComerciante, setComerciante] = useState({
+    nombre: "",
+    nombreUsuario: "",
+    contacto: "",
+    numeroPuesto: "",
+    nombreTienda: "",
+    informacionPuesto: "",
+    categoria: "",
+    imagen: ""
+  });
+  useEffect(() => {
+    console.log("Hola -------")
     let tienda_id = localStorage.getItem('myStore');
     let role = localStorage.getItem('myRole');
-    if(role === "comerciante"){
+    if(true){
       console.log("consulta sobre "+tienda_id);
       axios.get("/api/mostrar-detalles-tienda",
       {params:{
@@ -118,11 +29,12 @@ export default function EditarCuenta() {
       }
       ).then((res)=>{
         //console.log("obtenido: ");
-        console.log(res.data);
-
+        var data="";
+        console.log("Datos enviados desde back");
+        console.log(res.data)
         let datosTienda = res.data.response
         let comerciante = datosTienda.comercianteId
-        //console.log( "La tieenda \n", laTienda);
+        
         //console.log( "El comercianteeee \n", miComerciante);
         data = {
           nombre: comerciante.nombre,
@@ -134,65 +46,18 @@ export default function EditarCuenta() {
           categoria: datosTienda.categoria,
           imagen: datosTienda.imagen,
         };
-        console.log("back data");
-        console.log(data);
-       
-        return data;
-      })
-      .then((data)=>{
-        console.log("from second \n")
-        console.log(data);
-        setComerciante(data);
-      })
-      .catch(error => {
-        console.error('There was an error!', error);
-      });
-    }
-    console.log("Data returned")
-    console.log(data)
-    return data;
-  }
-  console.log(setter()); */
-  /* useEffect(() => {
-    
-    let tienda_id = localStorage.getItem('myStore');
-    let role = localStorage.getItem('myRole');
-    if(role === "comerciante"){
-      console.log("consulta sobre "+tienda_id);
-      axios.get("/api/mostrar-detalles-tienda",
-      {params:{
-      tiendaId : "600046678f25c125841686ad",
-      }
-      }
-      ).then((res)=>{
-        //console.log("obtenido: ");
-        console.log(res.data);
-
-        let datosTienda = res.data.response
-        let comerciante = datosTienda.comercianteId
-        //console.log( "La tieenda \n", laTienda);
-        //console.log( "El comercianteeee \n", miComerciante);
-        data = {
-          nombre: comerciante.nombre,
-          nombreUsuario: comerciante.nombreUsuario,
-          contacto: comerciante.contacto,
-          numeroPuesto: datosTienda.numeroPuesto,
-          nombreTienda: comerciante.nombreTienda,
-          informacionPuesto: datosTienda.informacionPuesto,
-          categoria: datosTienda.categoria,
-          imagen: datosTienda.imagen,
-        };
-        //console.log(data)
-        console.log(usuarioComerciante)
+        console.log( "La tieenda \n", data);
+        /*console.log(data)
+        console.log(usuarioComerciante)*/
         //usuarioComerciante = data;
-        //setComerciante(data);
+        setComerciante(data);
+        console.log("Usuario luego de set \n",usuarioComerciante);
       })
       .catch(error => {
         console.error('There was an error!', error);
       });
     }
-  }, []); */
-
+    }, [])
  /*  useEffect(() => {
     console.log("Cambio usuario comerciante")
     setComerciante(data);
@@ -202,7 +67,7 @@ export default function EditarCuenta() {
   return (
     
     <div>
-      <Test></Test>;
+      <FormularioComerciante comerciante={usuarioComerciante}></FormularioComerciante>
     </div>
   );
 }
